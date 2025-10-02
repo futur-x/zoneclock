b9d9e7372152d3009c050b63ce20efa4ea583779
futur-x <elttilz2@gmail.com>
2025-10-02 10:57:52 +0800

    Initial Commit

---
+++ zoneclock/zoneclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoneclockApp.swift
+//  zoneclock
+//
+//  Created by dajoe on 2025/10/2.
+//
+
+import SwiftUI
+
+@main
+struct zoneclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
